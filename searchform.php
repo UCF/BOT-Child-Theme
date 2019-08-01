5d85d32a6af27139608cb1fb21b4361250a5c3ec
RJ Bruneel <rj.bruneel@ucf.edu>
2019-08-01 09:40:05 -0400

    Added searh form

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="form-search" action="https://search.ucf.edu">
+	<div>
+		<div class="form-groupmb-0">
+			<input type="text" name="q" class="form-control custom-select-sm" id="q" placeholder="Search..." />
+			<input type="hidden" name="sitesearch" value="bot.ucf.edu">
+			<input type="submit" class="d-none">
+		</div>
+	</div>
+</form>
