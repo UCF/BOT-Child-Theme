2769c4508d7b3593c64e8f510865aac722b7d6d4
RJ Bruneel <rj.bruneel@ucf.edu>
2019-08-07 13:57:22 -0400

    Moved logic to BOT Utilites plugin

--- functions.php
+++ functions.php
@@ -6,19 +6,8 @@ include_once 'includes/meta.php';
 include_once 'includes/utilities.php';
 
 // Add other includes to this file as needed.
-include_once 'includes/shortcodes.php';
 include_once 'includes/meeting_functions.php';
 include_once 'includes/committee_functions.php';
 include_once 'includes/people.php';
 
-/**
- * Updates the people_group custom taxonomies labels
- **/
-function people_group_labels( $labels ) {
-	$labels['singular'] = 'Committee';
-	$labels['plural'] = 'Committees';
-	$labels['slug'] = 'committees';
-	return $labels;
-}
-
-add_filter( 'ucf_people_group_labels', 'people_group_labels', 10, 1 );
+?>
