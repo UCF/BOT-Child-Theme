c4781f36dd912e5be24a45b9153afa5541e5401e
RJ Bruneel <rj.bruneel@ucf.edu>
2019-08-07 09:11:39 -0400

    Added space between class names

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="form-search" action="https://search.ucf.edu">
 	<div>
-		<div class="form-groupmb-0">
+		<div class="form-group mb-0">
 			<input type="text" name="q" class="form-control custom-select-sm" id="q" placeholder="Search...">
 			<input type="hidden" name="sitesearch" value="bot.ucf.edu">
 			<input type="submit" class="d-none">
